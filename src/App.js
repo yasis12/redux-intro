2c3f8dbdda085f7dbf6961cc7d2855b17cbf3a79
Rafael Yasis <rafaelyasis@gmail.com>
2023-09-06 18:04:19 -0500

    learned about dispatch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 //redux
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
 
   //useSelector accepts a function that tells it what part of the store you want
   //here we return a whole store
   const reduxStore = useSelector(store => store)
+
+  //"dispatch" is how we talk to redux from react
+  const dispatch = useDispatch();
 
   //we want one thing
   const count = useSelector(store => store.count)
@@ -16,6 +19,12 @@ function App() {
     <div className="App">
       <br></br>
       <h2>{JSON.stringify(reduxStore)}</h2>
+
+      {/* dispatching an action when the button is clicked */}
+      <button onClick={() => dispatch({ type: 'INCREASE'})}>Increase</button>
+      <button onClick={() => dispatch({ type: 'DECREASE'})}>Decrease</button>
+
+      {/* Rendering the count from our Store */}
       <p>count is: {count}</p>
 
     </div>
--- src/index.js
+++ src/index.js
@@ -11,14 +11,26 @@ import {Provider} from 'react-redux'
 
 // old way let [count, setCount] = useState(0);
 //reducer
+
 const count = (state = 0, action) => {
   console.log('hi i\'m a reducer in count');
+  // it is a dev standard to have the type in all CAPS
+  if (action.type === 'INCREASE') {
+    console.log('you clicked increase');
+    // The next value of 'reduxStore.count' will be 1 more than previous value
+    return state + 1
+  } else if (action.type === 'DECREASE') {
+    // The next value of 'reduxStore.count' will be 1 less than previous value
+    return state - 1
+  }
+  //if action.type is anything else, just return the last known state
   return state
 };
 
 // New reducer
 const elementList = (state = [], action) => {
   console.log('hi i\'m a reducer in element list');
+
   return state
 };
 
@@ -33,8 +45,6 @@ const storeInstance = createStore(
   )
 
 );
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
